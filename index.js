cce6072d7bbf3df1cfc78aa953f42634b8d218c3
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-07-06 17:29:26 +0500

    npm error handled

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 5000;
 
 app.get("/", (req, res) => {
   console.log("HELLO");
   res.send("HEILLO");
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log("Port is listining at port 5000");
 });
