664ff603fa43f2fa2259d0609657e702849539d3
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-07-09 15:20:44 +0500

    error handling in dbconfig

--- dbconfig.js
+++ dbconfig.js
@@ -5,8 +5,17 @@ export async function Connect() {
   try {
     const connection = await mongoose.connect(process.env.MONGO_URL);
     console.log("MongoDB database connection established successfully");
+
+    mongoose.connection.on("connected", () => {
+      console.log("Connected");
+    });
+
+    mongoose.connection.on("error", (err) => {
+      console.log("Error in connection", err);
+      process.exit();
+    });
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
-    process.exit(1); // Exit the application on connection failure
+    process.exit(1);
   }
 }
