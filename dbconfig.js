e440cf30db652605b3186f9bac33b5d7ccccce37
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-07-09 15:12:52 +0500

    dbconnection completed

--- dbconfig.js
+++ dbconfig.js
@@ -1,4 +1,12 @@
-require("dotenv").config();
+import "dotenv/config";
 import mongoose from "mongoose";
 
-export async function Connect() {}
+export async function Connect() {
+  try {
+    const connection = await mongoose.connect(process.env.MONGO_URL);
+    console.log("MongoDB database connection established successfully");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1); // Exit the application on connection failure
+  }
+}
--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-require("dotenv").config();
-const express = require("express");
+import "dotenv/config";
+import express from "express";
+import mongoose from "mongoose";
+import { Connect } from "./dbconfig.js";
+
 const app = express();
+
+Connect();
+console.log("HELLO");
 
 app.get("/", (req, res) => {
   console.log("HELLO");
   res.send("HEILLO");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log("Port is listining at port 5000");
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server listening on port", process.env.PORT || 5000);
 });
